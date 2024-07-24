bcb820aba1de67fcfe084ba491ff46f7d1069daa
Ivan Chaman <ivanccg@outlook.com>
2024-07-24 12:48:07 -0600

    new login

--- Client_POS/Components/Pages/Login.razor.cs
+++ Client_POS/Components/Pages/Login.razor.cs
@@ -3,21 +3,30 @@ using Shelly.GraphQLCoreClient.Model;
 namespace ShellyPOS.Components.Pages
 {
     public partial class Login
-    {
-        public LoginData loginModel = new LoginData();
-        private async Task HandleLogin()
+    {          
+        private async Task OnLogin(LoginArgs args)
         {
-            var response = await LoginServices.Login(loginModel);            
+            var response = await LoginServices.Login(new LoginData() { User = args.Username, Password = args.Password });
             if (response.Result)
             {
                 var authStateProvider = (CustomAuthStateProvider)AuthenticationStateProvider;
-                await authStateProvider.MarkUserAsAuthenticated(loginModel.User);
+                await authStateProvider.MarkUserAsAuthenticated(response.Data.Uuid);
                 Navigation.NavigateTo("/");
             }
             else
-            {                
+            {
                 NotificationService.Notify(NotificationSeverity.Error, response.Errors[0].HeaderDefinition, response.Errors[0].DefaultMessage);
             }
+        }
+
+        private async Task OnRegister()
+        {
+
+        }
+
+        private async Task OnResetPassword(string args)
+        {
+
         }
     }
 }
